c048f116e33265eb4d5dff09a467714313092cd5
takagisou <takagisou@outlook.com>
2017-11-08 03:57:30 +0900

    HTMLエディタ作成

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,23 @@
+import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.web.HTMLEditor
+import javafx.stage.Stage
+
+object Main extends App {
+    Application.launch(classOf[Main], args: _*)
+}
+
+class Main extends Application {
+
+    override def start(primaryStage: Stage): Unit = {
+        primaryStage.setTitle("HTML Editor")
+        primaryStage.setWidth(1000)
+        primaryStage.setHeight(600)
+
+        val htmlEditor = new HTMLEditor()
+        htmlEditor.setPrefHeight(245)
+        val scene = new Scene(htmlEditor)
+        primaryStage.setScene(scene)
+        primaryStage.show()
+    }
+}
