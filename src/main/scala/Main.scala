14d3452537630cdd5ab0fb4623513f0c4fe5ad25
Kazuyuki Matsuda <mtdkzyk.github@gmail.com>
2020-02-24 20:19:02 +0900

    Add sample text

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -17,5 +17,8 @@ class HTMLEditorSample extends Application {
     primaryStage.setTitle("HTMLEditor Sample")
     primaryStage.setScene(scene)
     primaryStage.show()
+
+    val INITIAL_TEXT = "<html><body>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tortor felis, pulvinar in scelerisque cursus, pulvinar at ante. Nulla consequatcongue lectus in sodales. Nullam eu est a felis ornare bibendum et nec tellus. Vivamus non metus tempus augue auctor ornare. Duis pulvinar justo ac purus adipiscing pulvinar. Integer congue faucibus dapibus. Integer id nisl ut elit aliquam sagittis gravida eu dolor. Etiam sit amet ipsum sem.</body></html>"
+    htmlEditor.setHtmlText(INITIAL_TEXT)
   }
 }
